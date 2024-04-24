888d7083585bdfa40ca4029d0ec96d77acc2f408
AndreyBakulev <gengubae@gmail.com>
2024-04-24 14:15:57 -0400

    initial commit

---
+++ src/huffman.rs
@@ -0,0 +1,18 @@
+pub struct Huffman{
+
+}
+impl Huffman{
+    pub fn new(){
+
+    }
+    pub fn encode(string: String){
+        /*
+        1. get amt of each char
+        then make a btree based off char combinations
+        plot 0's and 1's
+        make dictionary based off binary val + char
+        read the string again and translate it to the binary string
+        finish :D
+        */
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod huffman;
+
 fn main() {
     println!("Hello, world!");
 }
+/*
+HOW TO:
+get total amt of each char in a given string
+combine two least seen char's into a string "c + b -> cb | value
+make a dict for each char and occurences
+now do it again (combine two least common) e + cb -> ecb | value
+do this until you have one string
+this should be a binary tree of the single characters
+
+each time there is a left, type 0
+each time right, type 1
+trace a path using 1's and 0's to each char (c = 1110)
+concat back into string of 0's and 1's
+ */
