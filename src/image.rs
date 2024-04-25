f7bf671049d203d3fad2004b57b1efc0b37df80b
AndreyBakulev <gengubae@gmail.com>
2024-04-25 16:37:40 -0400

    adding more texts

--- src/image.rs
+++ src/image.rs
@@ -1,83 +1,83 @@
-use std::fmt;
-
-pub struct Image {
-    width: usize,
-    height: usize,
-    pixels: Vec<u8>,
-}
-
-impl Image {
-    pub fn new(width: usize, height: usize, pixels: Vec<u8>) -> Self {
-        Image {
-            width,
-            height,
-            pixels,
-        }
-    }
-    pub fn save(&self, path: &str) -> Result<(), std::io::Error> {
-        //image::save_buffer(path, &self.pixels, self.width, self.height, image::ColorType::Rgb8)?;
-        unimplemented!()
-    }
-
-    pub fn compress_rle(&self) -> Vec<(u8, u32)> {
-        run_length_encode(&self.pixels)
-    }
-
-    pub fn decompress_rle(encoded: &[(u8, u32)]) -> Self {
-        let pixels = run_length_decode(encoded);
-        Image {
-            width: 0,
-            height: 0,
-            pixels,
-        }
-    }
-    pub fn compress_dct(&self, quality: f64) -> Vec<f64> {
-        compress_image(&self.pixels, self.width, self.height, quality)
-    }
-    pub fn decompress_dct(compressed: &[f64], width: usize, height: usize) -> Self {
-        let pixels = decompress_image(compressed, width, height);
-        Image {
-            width,
-            height,
-            pixels: pixels.iter().map(|&x| x as u8).collect(),
-        }
-    }
-}
-
-fn run_length_encode(pixels: &[u8]) -> Vec<(u8, u32)> {
-    //basically check if current pixel is same as prev pixel
-    //if yes, add count to u32, if no, push and go to next pixel
-    let mut encoded = Vec::new();
-    let mut count = 1;
-    let mut prev_pixel = pixels[0];
-    for &pixel in pixels.iter().skip(1) {
-        if pixel == prev_pixel {
-            count += 1;
-        } else {
-            encoded.push((prev_pixel, count));
-            prev_pixel = pixel;
-            count = 1;
-        }
-    }
-    encoded.push((prev_pixel, count));
-    encoded
-}
-
-fn run_length_decode(encoded: &[(u8, u32)]) -> Vec<u8> {
-    //fill count amt of pixels with u8 color
-    let mut decoded = Vec::new();
-    for &(pixel,count) in encoded {
-        decoded.extend(std::iter::repeat(pixel).take(count as usize));
-    }
-    decoded
-}
-
-fn compress_image(pixels: &[u8], width: usize, height: usize, quality: f64) -> Vec<f64> {
-    // Implementation of DCT compression
-    // ...
-}
-
-fn decompress_image(compressed: &[f64], width: usize, height: usize) -> Vec<f64> {
-    // Implementation of DCT decompression
-    // ...
-}
+// use std::fmt;
+//
+// pub struct Image {
+//     width: usize,
+//     height: usize,
+//     pixels: Vec<u8>,
+// }
+//
+// impl Image {
+//     pub fn new(width: usize, height: usize, pixels: Vec<u8>) -> Self {
+//         Image {
+//             width,
+//             height,
+//             pixels,
+//         }
+//     }
+//     pub fn save(&self, path: &str) -> Result<(), std::io::Error> {
+//         //image::save_buffer(path, &self.pixels, self.width, self.height, image::ColorType::Rgb8)?;
+//         unimplemented!()
+//     }
+//
+//     pub fn compress_rle(&self) -> Vec<(u8, u32)> {
+//         run_length_encode(&self.pixels)
+//     }
+//
+//     pub fn decompress_rle(encoded: &[(u8, u32)]) -> Self {
+//         let pixels = run_length_decode(encoded);
+//         Image {
+//             width: 0,
+//             height: 0,
+//             pixels,
+//         }
+//     }
+//     pub fn compress_dct(&self, quality: f64) -> Vec<f64> {
+//         compress_image(&self.pixels, self.width, self.height, quality)
+//     }
+//     pub fn decompress_dct(compressed: &[f64], width: usize, height: usize) -> Self {
+//         let pixels = decompress_image(compressed, width, height);
+//         Image {
+//             width,
+//             height,
+//             pixels: pixels.iter().map(|&x| x as u8).collect(),
+//         }
+//     }
+// }
+//
+// fn run_length_encode(pixels: &[u8]) -> Vec<(u8, u32)> {
+//     //basically check if current pixel is same as prev pixel
+//     //if yes, add count to u32, if no, push and go to next pixel
+//     let mut encoded = Vec::new();
+//     let mut count = 1;
+//     let mut prev_pixel = pixels[0];
+//     for &pixel in pixels.iter().skip(1) {
+//         if pixel == prev_pixel {
+//             count += 1;
+//         } else {
+//             encoded.push((prev_pixel, count));
+//             prev_pixel = pixel;
+//             count = 1;
+//         }
+//     }
+//     encoded.push((prev_pixel, count));
+//     encoded
+// }
+//
+// fn run_length_decode(encoded: &[(u8, u32)]) -> Vec<u8> {
+//     //fill count amt of pixels with u8 color
+//     let mut decoded = Vec::new();
+//     for &(pixel,count) in encoded {
+//         decoded.extend(std::iter::repeat(pixel).take(count as usize));
+//     }
+//     decoded
+// }
+//
+// fn compress_image(pixels: &[u8], width: usize, height: usize, quality: f64) -> Vec<f64> {
+//     // Implementation of DCT compression
+//     // ...
+// }
+//
+// fn decompress_image(compressed: &[f64], width: usize, height: usize) -> Vec<f64> {
+//     // Implementation of DCT decompression
+//     // ...
+// }
--- target/debug/deps/Huffman_coding-8eeba348e424b0b1.d
+++ target/debug/deps/Huffman_coding-8eeba348e424b0b1.d
@@ -1,7 +1,7 @@
-/Users/macpro/Documents/GitHub/RustRover/huffman_coding/target/debug/deps/Huffman_coding-8eeba348e424b0b1: src/main.rs src/huffman.rs src/image_compression.rs
+/Users/macpro/Documents/GitHub/RustRover/huffman_coding/target/debug/deps/Huffman_coding-8eeba348e424b0b1: src/main.rs src/huffman.rs src/image.rs
 
-/Users/macpro/Documents/GitHub/RustRover/huffman_coding/target/debug/deps/Huffman_coding-8eeba348e424b0b1.d: src/main.rs src/huffman.rs src/image_compression.rs
+/Users/macpro/Documents/GitHub/RustRover/huffman_coding/target/debug/deps/Huffman_coding-8eeba348e424b0b1.d: src/main.rs src/huffman.rs src/image.rs
 
 src/main.rs:
 src/huffman.rs:
-src/image_compression.rs:
+src/image.rs:
